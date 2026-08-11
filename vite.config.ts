d3eb5cf2a1708edfdcaa8346aedecd8f9596bb20
zero-grain <164316700+zero-grain@users.noreply.github.com>
2026-08-11 12:57:38 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  // Relative assets work both at a custom domain and /repository/ on GitHub Pages.
-  base: "./",
-  plugins: [react()],
-});
